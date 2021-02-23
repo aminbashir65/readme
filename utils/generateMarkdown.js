89b0efaa1e3783ae25c6776410fcf5b0860dea55
amin bashir <aminbashir65@gmail.com>
2021-02-23 13:32:57 -0600

    made a license badme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,10 @@
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseBadge(license) { 
-  
+  if (license !== 'none') {
+    return `![License:](https://img.shields.io/badge/License-${license}-yellow.svg)`
+}
+return '';
 }
 
 // TODO: Create a function to generate markdown for README
