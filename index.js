6ab78d94bc06212287515a3a80c1033183012c5d
amin bashir <aminbashir65@gmail.com>
2021-02-23 13:05:46 -0600

    created all inputs

--- index.js
+++ index.js
@@ -5,16 +5,57 @@ const generateMarkdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
 const questions = [
     {
-        name:"title",
         type: "input",
-        message: "what is the title",
-    }
+        name:"title",
+        message: "what is the title of the Project",
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Describe the project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter any special usage instructions.'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license would you like to use?',
+        choices: ['Apache 2.0', 'GNU', 'MIT', 'Creative Commons', 'None']
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your github username?'
+    },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     const markDown = generateMarkdown(data)
-    console.log(markDown)
+    // console.log(markDown)
     fs.writeFile(`output/${fileName}`, markDown, function(err){
         if (err) console.log(err)
     } )
